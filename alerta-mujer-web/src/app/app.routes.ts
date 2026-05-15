0dea263356501cc296f43a26db713107eb132d02
Maicol <Samaicol777@gmail.com>
2026-05-15 17:42:37 -0500

    feat: initial structure including main project folder

---
+++ alerta-mujer-web/src/app/app.routes.ts
@@ -0,0 +1,30 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./features/auth/login/login').then(
+        (m) => m.Login
+      ),
+  },
+
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./features/dashboard/home/home').then(
+        (m) => m.Home
+      ),
+  },
+
+  {
+    path: '**',
+    redirectTo: 'login',
+  },
+];
